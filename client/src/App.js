83e90c48b415e4f1e1cec954e5c66513651b1c8d
Raj Kishor <mr.kishorraj@gmail.com>
2021-04-25 17:17:05 +0530

    React app created

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App"> 
     <h1>Movie Reviews</h1>
+    <input type="text" name="MovieName" />
+    <input type="text" name="review" /> 
     </div>
   );
 }
